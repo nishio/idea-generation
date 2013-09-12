a9a2f2de5604d75216fe6024d8b9d1c9c02e4fc7
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-09-12 21:29:50 +0900

    add log.js and notify.js

---
+++ common/nhiro/notify.js
@@ -0,0 +1,50 @@
+/**
+ * (c) 2013, Cybozu.
+ * Growl-like notification.
+ * Require: jQuery, jQuery UI Dialog
+ */
+goog.provide('nhiro.notify');
+
+/**
+ * @param {string} message .
+ * @param {number=} seconds .
+ * @suppress {checkTypes}
+ */
+nhiro.notify = function(message, seconds) {
+    var $ = nhiro.repos.get('jQuery');
+    var box = $('<div>');
+    if (seconds == null) seconds = 10;
+
+    box.text(message);
+    box.appendTo('body');
+    var position, onClose;
+    if (nhiro.notify.top_filled) {
+        position = {my: 'right top', at: 'right bottom', of: nhiro.notify.last};
+        onClose = function() {};
+    }else {
+        position = {my: 'right top', at: 'right top', of: $('body')};
+        nhiro.notify.top_filled = true;
+        onClose = function() {
+            nhiro.notify.top_filled = false;
+        };
+    }
+    var dialog = box.dialog({
+        position: position,
+        resizable: false,
+        draggable: false,
+        show: 'fade',
+        hide: 'fade',
+        close: onClose
+    });
+
+    nhiro.notify.last = dialog;
+    setTimeout(function() {
+        dialog.dialog('close');
+        setTimeout(function() {
+            dialog.dialog('destroy');
+        }, 1000);
+    }, 1000 * seconds);
+};
+
+nhiro.notify.top_filled = false;
+nhiro.notify.last = null;
