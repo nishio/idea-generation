fe866ebf95cb5e5c17bed40a08cee8849bffeef7
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-10-25 16:58:31 +0900

    add feature to add item

--- collecting/js/main.js
+++ collecting/js/main.js
@@ -1,4 +1,64 @@
 //localStorage.setItem('collecting_ideas', []);
-$(function(){
+var items;
+
+function updateJSON(){
+    var is_multiline = $('#json_multiline').prop('checked');
+    var json = '[';
+    var first = true;
+    items.forEach(function(item){
+        if(!first){
+            json += ',';
+        }
+        if(is_multiline) json += '\n  ';
+        json += JSON.stringify(item);
+        first = false;
+    })
+    if(is_multiline) json += '\n';
+    json += ']';
+    $('#json').val(json);
+}
+
+function update(){
+    var text = $('#text').val();
+    var id = items.length;
+    var when = new Date().toISOString();
+    var item = {
+        'text': text,
+        'when': when,
+        'id': id};
     
-})
+
+    items.push(item);
+
+    // clear
+    $('#text').val('');
+
+    // update JSON
+    updateJSON();
+}
+
+$(function(){
+    items = localStorage.getItem('collecting_ideas');
+    if(items == null || items == ""){
+        items = [];
+    }
+
+
+    $('input[type=text]').keypress(function(e) {
+        if (e.keyCode == 13) update();
+    });
+    $('#text').keypress(function(e) {
+        if (e.keyCode == 13){
+            update();
+            return false;
+        }
+    });
+
+    $('#json_multiline').click(function(){
+        updateJSON();
+    })
+
+    updateJSON()
+});
+
+    //localStorage.clear()
