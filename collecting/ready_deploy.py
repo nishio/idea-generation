19d8f930d219deb1002480d58a3b4e680e0bb91d
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-11-09 00:47:00 +0900

    to avoid forgetting to change version number, change it when deploy automatically

--- collecting/ready_deploy.py
+++ collecting/ready_deploy.py
@@ -1,5 +1,14 @@
 import os
+import datetime
 dir = os.path.abspath(os.path.dirname(__file__))
+
 data = file(os.path.join(dir, 'index.html')).read()
 data = data.replace('<html>', '<html manifest="manifest.webapp">')
-print data
+file(os.path.join(dir, 'deploy/index.html'), 'w').write(data)
+
+
+data = file(os.path.join(dir, 'manifest.webapp')).read()
+now = datetime.datetime.now().isoformat()
+data = data.replace('# version', '# version: ' + now)
+file(os.path.join(dir, 'deploy/manifest.webapp'), 'w').write(data)
+
