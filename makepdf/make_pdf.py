a42c7d8fdf3643a2db813b23f98af9fd1816b608
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2014-07-11 17:18:16 +0900

    refactor, draw line, fix bug

--- makepdf/make_pdf.py
+++ makepdf/make_pdf.py
@@ -13,6 +13,54 @@ from reportlab.lib.pagesizes import landscape, A4
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 
+fontname = "HeiseiKakuGo-W5"
+pdfmetrics.registerFont(UnicodeCIDFont(fontname))
+
+
+style = getSampleStyleSheet()['Normal']
+style.fontName = fontname
+style.wordWrap = "CJK"
+pagesize = landscape(A4)
+
+WIDTH, HEIGHT = pagesize
+NUM = 5
+PADDING_PAGE = 30  # padding between page boundary and contents
+MARGIN_BOX = 20  # margin between boxs
+
+WIDTH_BOX = (WIDTH - 2 * PADDING_PAGE - 4 * MARGIN_BOX) / 5
+HEIGHT_BOX = (HEIGHT - 2 * PADDING_PAGE - 4 * MARGIN_BOX) / 5
+START_FONT_SIZE = 30
+
+def calc_xy(i, j):
+    x = PADDING_PAGE + (WIDTH_BOX + MARGIN_BOX) * i
+    y = PADDING_PAGE + (HEIGHT_BOX + MARGIN_BOX) * j
+    return x, y
+
+
+def make_para(text):
+    "make Paragraph object which fit in given box"
+    style.fontSize = START_FONT_SIZE
+    style.leading = START_FONT_SIZE
+    while True:
+        para = Paragraph(text, style)
+        w, h = para.wrap(WIDTH_BOX, HEIGHT_BOX)
+        if w > WIDTH_BOX or h > HEIGHT_BOX:
+            style.fontSize -= 1
+            style.leading -= 1
+            continue
+
+        # in some case of 禁則処理 overflow can happen
+        para2 = Paragraph(text, style)
+        lines = para2.breakLinesCJK([WIDTH_BOX, WIDTH_BOX]).lines
+        try:
+            if any(rest < 0 for rest, _text in lines):
+                style.fontSize -= 1
+                style.leading -= 1
+                continue
+        except:
+            print lines
+        break
+    return para
 
 def get_items(files=[]):
     if files == []:
@@ -26,72 +74,47 @@ def get_items(files=[]):
     for filename in files:
         newitems = json.load(file(filename))
         items.extend(newitems)
+
+    items = [x['text'] for x in items]
     return items
 
-items = get_items()
-if len(items) < 25:
-    items.extend([{'text': ''}] * (25 - len(items)))
+
+def make_pdf(items=None):
+    if not items: items = get_items()
+    c = canvas.Canvas("hello.pdf", pagesize=pagesize)
+
+    c.saveState()
+
+    from math import ceil
+    NUM_PAGES = int(ceil(len(items) / 25.0))
+    for pages in range(NUM_PAGES):
+        for i in range(6):
+            for j in range(6):
+                x, y = calc_xy(i, j)
+                c.setStrokeColorRGB(0.8, 0.8, 0.8)
+                c.setLineWidth(MARGIN_BOX / 2)
+                c.line(0, y - MARGIN_BOX / 2, WIDTH, y - MARGIN_BOX / 2)
+                c.line(x - MARGIN_BOX / 2, 0, x - MARGIN_BOX / 2, HEIGHT)
+
+        for i in range(5):
+            for j in range(5):
+                card_index = pages * 25 + i * 5 + j
+                if card_index >= len(items): continue  # no more cards
+
+                text = items[card_index]
+
+                para = make_para(text)
+                x, y = calc_xy(i, j)
+                para.drawOn(c, x, y + HEIGHT_BOX - para.height)
+
+        c.showPage()
+
+    c.save()
+
+def main():
+    make_pdf()
 
 
-fontname = "HeiseiKakuGo-W5"
-pdfmetrics.registerFont(UnicodeCIDFont(fontname))
 
-
-style = getSampleStyleSheet()['Normal']
-style.fontName = fontname
-style.wordWrap = "CJK"
-pagesize = landscape(A4)
-c = canvas.Canvas("hello.pdf", pagesize=pagesize)
-
-c.saveState()
-#c.setFont("HeiseiKakuGo-W5",18)
-
-#canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-70, Title)
-WIDTH, HEIGHT = pagesize
-NUM = 5
-PADDING_PAGE = 30  # padding between page boundary and contents
-MARGIN_BOX = 20  # margin between boxs
-
-WIDTH_BOX = (WIDTH - 2 * PADDING_PAGE - 4 * MARGIN_BOX) / 5
-HEIGHT_BOX = (HEIGHT - 2 * PADDING_PAGE - 4 * MARGIN_BOX) / 5
-START_FONT_SIZE = 30
-from math import ceil
-NUM_PAGES = int(ceil(len(items) / 25.0))
-for pages in range(NUM_PAGES):
-    for i in range(5):
-        for j in range(5):
-            x = PADDING_PAGE + (WIDTH_BOX + MARGIN_BOX) * i
-            y = PADDING_PAGE + (HEIGHT_BOX + MARGIN_BOX) * j
-            text = items[pages * 25 + i * 5 + j]['text']
-            if 0:
-                c.line(0, y, WIDTH, y)
-                c.line(x, 0, x, HEIGHT)
-                c.line(0, y + HEIGHT_BOX, WIDTH, y + HEIGHT_BOX)
-                c.line(x + WIDTH_BOX, 0, x + WIDTH_BOX, HEIGHT)
-
-            style.fontSize = START_FONT_SIZE
-            style.leading = START_FONT_SIZE
-            while True:
-                para = Paragraph(text, style)
-                w, h = para.wrap(WIDTH_BOX, HEIGHT_BOX)
-                if w > WIDTH_BOX or h > HEIGHT_BOX:
-                    style.fontSize -= 1
-                    style.leading -= 1
-                    continue
-
-                # in some case of 禁則処理 overflow can happen
-                para2 = Paragraph(text, style)
-                lines = para2.breakLinesCJK([WIDTH_BOX, WIDTH_BOX]).lines
-                if any(rest < 0 for rest, _text in lines):
-                    style.fontSize -= 1
-                    style.leading -= 1
-                    continue
-
-                break
-
-            para.drawOn(c, x, y + HEIGHT_BOX - h)
-
-            #c.drawString(x, y, text)
-    c.showPage()
-
-c.save()
+if __name__ == '__main__':
+    main()
