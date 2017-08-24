8753c0dcb13c979312b2785274e21dceb0e7f1fa
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2017-08-24 17:03:01 +0900

    update

--- from_pdf/from_pdf.py
+++ from_pdf/from_pdf.py
@@ -8,24 +8,30 @@ It uses PDFMiner's pdf2txt.py
 """
 import os
 import re
+import sys
 import subprocess
 from neologdn import normalize
+import time
+import argparse
+import shutil
 
-# TODO: 入出力場所を指定できるようにする
-INDIR = 'in/'
-OUTDIR = 'out/'
+parser = argparse.ArgumentParser(description='Convert PDF to movie.')
+parser.add_argument('--indir', action='store', default='in',
+                   help='directory for PDFs')
+parser.add_argument('--outdir', action='store', default='out',
+                   help='directory to output PNGs')
 
-for filename in sorted(os.listdir(INDIR)):
-    # TODO: omit_pattern を指定できるようにする
-    #if filename.startswith('iphone4_'): continue
-    if filename.startswith('.'): continue
-    if not filename.endswith('.pdf'): continue
-    print(filename)
-    outfile = os.path.join(OUTDIR, filename).replace('.pdf', '.txt')
+args = parser.parse_args()
+
+
+def to_text(filename):
+    outdir = os.path.join(args.outdir, filename).replace('.pdf', '')
+    outfile = os.path.join(outdir, "line_per_page.txt")
     if os.path.isfile(outfile):
-        print 'already exists'
-        continue
-    infile = os.path.join(INDIR, filename)
+        print 'text already exists'
+        return
+
+    infile = os.path.join(args.indir, filename)
     subprocess.check_call(['pdf2txt.py', '-V', '-o', 'tmp.txt', infile])
 
 
@@ -33,3 +39,36 @@ for filename in sorted(os.listdir(INDIR)):
     data = data.split('\x0c')
     data = [normalize(page.replace('\n', ' ').decode('utf-8')) for page in data]
     file(outfile, 'w').write('\n'.join(data).encode('utf-8'))
+
+
+def to_pngs(filename):
+    outdir = os.path.join(args.outdir, filename).replace('.pdf', '')
+    if os.path.isdir(outdir):
+        print 'folder already exists'
+        return
+
+    infile = os.path.join(args.indir, filename)
+    print 'extracting pages'
+    shutil.rmtree('tmp', ignore_errors=True)
+    os.makedirs('tmp')
+    starttime = time.time()
+
+    subprocess.check_call(
+        ['pdftocairo', '-r', '45', '-f', '0',
+         #'-l', '10',
+         '-png', infile, 'tmp/page'],
+        stdout=sys.stdout, stderr=sys.stderr)
+
+    print 'elapse', time.time() - starttime
+    shutil.move('tmp', outdir)
+
+
+for filename in sorted(os.listdir(args.indir)):
+    # TODO: omit_pattern を指定できるようにする
+    #if filename.startswith('iphone4_'): continue
+    if filename.startswith('.'): continue
+    if not filename.endswith('.pdf'): continue
+    print(filename)
+
+    to_text(filename)
+    to_pngs(filename)
